44468bda2b9eda37bf9f6d522e3c3d365130d23c
Advertikon <bogochunas@gmail.com>
2017-06-01 17:03:23 +0300

    update

--- main.c
+++ main.c
@@ -22,13 +22,18 @@ int add_list_item( char *name, char *value, struct list *this ) {
 	struct list *last;
 	last = this;
 
-	while( last->next ) {
+	while( last->name ) {
 		last = last->next;
 	}
 
 	last->next = malloc( sizeof( struct list ) );
-	last->next->name = name;
-	last->next->value = value;
+	last->name = malloc( strlen( name ) );
+	strcpy( last->name, name );
+	last->value = malloc( strlen( value ) );
+	strcpy( last->value, value );
+
+	printf( "New member pointer: %p\n", last );
+	printf( "New member structure: name = %s, value = %s\n", last->name, last->value );
 
 	return 0;
 }
@@ -38,14 +43,15 @@ int get_list_item( char *name, char *value, struct list *this ) {
 	current = this;
 	int found = 0;
 
-	while ( current->next ) {
+	do {
 		if ( !strcmp( current->name, name ) ) {
 			found = 1;
 			break;
 		}
 
-		current = current->next;
-	}
+		// current = current->next;
+
+	} while ( current = current->next );
 
 	if ( found ) {
 		value = current->value;
@@ -53,6 +59,22 @@ int get_list_item( char *name, char *value, struct list *this ) {
 
 	return found ? 0 : 1;
 
+}
+
+int print_args( struct list* l ) {
+	struct list *ll;
+	ll = l;
+
+	printf( "Current member to search position: %p\n", ll );
+
+	// printf( "Current structure to print: %s = %s\n", ll->name, ll->value );
+
+	while( ll->name ) {
+		printf( "Current structure to print: %s = %s\n", ll->name, ll->value );
+		ll = ll->next;
+	}
+
+	return 0;
 }
 
 // struct list {
@@ -81,23 +103,25 @@ int usage() {
 int parse_args( char **args ) {
 	char *p1;
 	char p2;
-	char *a_name = NULL;
-	char *a_val = NULL;
+	char a_name[100];
+	char a_value[100];
 	int in_arg = 0;
 	int in_long_arg = 0;
 	int in_name = 0;
 	int in_value = 0;
 
+	a_name[ 0 ] = '\n';
+
 	args++; /* Skip program invocation name */
 
 	while( p1 = *args++ ) {
 		printf( "New arg\n" );
+		printf( "A_name: %s\n", a_name );
 
 		while( p2 = *p1++ ) {
 			printf( "New char\n" );
 
 			if ( '-' == p2 ) {
-
 				printf( "Dash\n" );
 
 				if ( in_arg ) {
@@ -122,12 +146,12 @@ int parse_args( char **args ) {
 					if ( in_value ) {
 						printf( "In value\n" );
 
-						if ( !a_name ) {
+						if ( !strlen( a_name ) ) {
 							print_error( "Undefined argument %s", p1 - 1 );
 						}
 
 						add_list_item( a_name, "1", &a_list );
-						a_name = &p2;
+						strcpy( a_name, &p2 );
 						a_name[ 1 ] = '\0';
 						printf( "Set name to: %s\n", a_name );
 					}
@@ -135,14 +159,14 @@ int parse_args( char **args ) {
 					if ( in_long_arg ) {
 						printf( "In long argument\n" );
 
-						a_name = &p2;
+						strcpy( a_name, &p2 );
 						printf( "Set name to: %s\n", a_name );
 						break;
 
 					} else {
 						printf( "In short argument\n" );
 
-						a_name = &p2;
+						strcpy( a_name, &p2 );
 						a_name[ 1 ] = '\0';
 						printf( "Set name to: %s\n", a_name );
 						in_value = 1;
@@ -151,12 +175,14 @@ int parse_args( char **args ) {
 				} else {
 					printf( "Value part\n" );
 
-					if ( !a_name  ) {
+					if ( !strlen( a_name )  ) {
 						print_error( "Undefined argument %s", p1 - 1 );
 					}
 
-					add_list_item( a_name, &p2, &a_list );
-					a_name = NULL;
+					printf( "Name to be added: %s\n", a_name );
+
+					add_list_item( a_name, strcpy( a_value, p1 - 1 ), &a_list );
+					a_name[ 0 ] = '\n';
 					printf( "Set name to: %s\n", "NULL" );
 					break;
 				}
@@ -166,8 +192,6 @@ int parse_args( char **args ) {
 		in_value = 0;
 		in_arg = 0;
 		in_long_arg = 0;
-		
-		printf("\n" );
 	}
 }
 
@@ -181,15 +205,4 @@ int print_error( char * msg, ... ) {
 	va_end( args );
 	
 	exit( 1 );
-}
-
-int print_args( struct list* l ) {
-	printf( "%s = %s\n", l->name, l->value );
-
-	while( l->next ) {
-		l = l->next;
-		printf("%s = %s\n", l->name, l->value );
-	}
-
-	return 0;
 }
