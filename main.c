4fbec2cfec74a901279a4c41f001c65c6a8c020f
Maxim Bogochunas <max@localhost.localdomain>
2017-06-01 22:42:02 +0300

    arumants parser

--- main.c
+++ main.c
@@ -14,11 +14,10 @@ struct list {
 };
 
 int add_list_item( char*, char*, struct list* );
-int get_list_item( char*, char*, struct list* );
+int get_list_item( char*, char**, struct list* );
 int print_args( struct list* );
 
 int add_list_item( char *name, char *value, struct list *this ) {
-	printf( "Add to structure %s = %s\n", name, value );
 	struct list *last;
 	last = this;
 
@@ -32,13 +31,10 @@ int add_list_item( char *name, char *value, struct list *this ) {
 	last->value = malloc( strlen( value ) );
 	strcpy( last->value, value );
 
-	printf( "New member pointer: %p\n", last );
-	printf( "New member structure: name = %s, value = %s\n", last->name, last->value );
-
 	return 0;
 }
 
-int get_list_item( char *name, char *value, struct list *this ) {
+int get_list_item( char *name, char** value, struct list *this ) {
 	struct list *current;
 	current = this;
 	int found = 0;
@@ -49,25 +45,20 @@ int get_list_item( char *name, char *value, struct list *this ) {
 			break;
 		}
 
-		// current = current->next;
+		current = current->next;
 
-	} while ( current = current->next );
+	} while ( current->name );
 
 	if ( found ) {
-		value = current->value;
+		*value = current->value;
 	}
 
 	return found ? 0 : 1;
-
 }
 
 int print_args( struct list* l ) {
 	struct list *ll;
 	ll = l;
-
-	printf( "Current member to search position: %p\n", ll );
-
-	// printf( "Current structure to print: %s = %s\n", ll->name, ll->value );
 
 	while( ll->name ) {
 		printf( "Current structure to print: %s = %s\n", ll->name, ll->value );
@@ -77,20 +68,29 @@ int print_args( struct list* l ) {
 	return 0;
 }
 
-// struct list {
-// 	struct list_item first;
-// 	int( *add )( char*, char*, struct list );
-// 	int( *get )( char*, char* );
-// };
-
 char *e_mess = "";
 char *name = "Crawler";
 struct list a_list;
 
 int main( int argc, char **argv ) {
+	char* value;
 
 	parse_args( argv );
 	print_args( &a_list );
+
+	if ( 0 == get_list_item( "bb", &value, &a_list ) ) {
+		printf( "bb = %s\n", value );
+
+	} else {
+		printf( "bb has no value\n" );
+	}
+
+	if ( 0 == get_list_item( "s", &value, &a_list ) ) {
+		printf( "s = %s\n", value );
+
+	} else {
+		printf( "s has no value\n" );
+	}
 
 	return 0;
 }
@@ -107,91 +107,94 @@ int parse_args( char **args ) {
 	char a_value[100];
 	int in_arg = 0;
 	int in_long_arg = 0;
-	int in_name = 0;
 	int in_value = 0;
+	int has_name = 0;
+	int has_value = 0;
+	int last_arg = 0;
 
 	a_name[ 0 ] = '\n';
 
 	args++; /* Skip program invocation name */
 
 	while( p1 = *args++ ) {
-		printf( "New arg\n" );
-		printf( "A_name: %s\n", a_name );
-
 		while( p2 = *p1++ ) {
-			printf( "New char\n" );
 
 			if ( '-' == p2 ) {
-				printf( "Dash\n" );
-
 				if ( in_arg ) {
-					printf( "Dash-dash\n" );
-
 					in_long_arg = 1;
 
 				} else {
 					in_arg = 1;
 				}
 
-			} else if ( '"' == p2 || '\'' == p2 ) {
+				if ( has_name ) {
+					if ( last_arg >= 2 ) {
+						print_error( "Option %s has no value", a_name );
+					}
 
+					if ( last_arg ) {
+						add_list_item( a_name, "1", &a_list );
+						has_name = 0;
+						has_value = 0;
+					}
+				}
+
+			} else if ( '"' == p2 || '\'' == p2 ) {
 				// skip
 
 			} else {
-				printf( "Alpha: %c\n", p2 );
-
 				if ( in_arg ) {
-					printf( "In argument\n" );
-
 					if ( in_value ) {
-						printf( "In value\n" );
-
-						if ( !strlen( a_name ) ) {
+						if ( !has_name ) {
 							print_error( "Undefined argument %s", p1 - 1 );
 						}
 
 						add_list_item( a_name, "1", &a_list );
-						strcpy( a_name, &p2 );
-						a_name[ 1 ] = '\0';
-						printf( "Set name to: %s\n", a_name );
+						has_value = 0;
+						a_name[ 0 ] = p2;
+						has_name = 1;
 					}
 
 					if ( in_long_arg ) {
-						printf( "In long argument\n" );
-
-						strcpy( a_name, &p2 );
-						printf( "Set name to: %s\n", a_name );
+						strcpy( a_name, p1 - 1 );
+						has_name = 1;
 						break;
 
 					} else {
-						printf( "In short argument\n" );
-
-						strcpy( a_name, &p2 );
+						a_name[ 0 ] = p2;
 						a_name[ 1 ] = '\0';
-						printf( "Set name to: %s\n", a_name );
+						has_name = 1;
 						in_value = 1;
 					}
 
 				} else {
-					printf( "Value part\n" );
-
-					if ( !strlen( a_name )  ) {
+					if ( !has_name  ) {
 						print_error( "Undefined argument %s", p1 - 1 );
 					}
 
-					printf( "Name to be added: %s\n", a_name );
-
 					add_list_item( a_name, strcpy( a_value, p1 - 1 ), &a_list );
-					a_name[ 0 ] = '\n';
-					printf( "Set name to: %s\n", "NULL" );
+					has_name = 0;
+					has_value = 0;
 					break;
 				}
 			}
 		}
 
+		last_arg = in_arg + in_long_arg * 2;
+
 		in_value = 0;
 		in_arg = 0;
 		in_long_arg = 0;
+	}
+
+	if ( has_name ) {
+		if ( last_arg >= 2 ) {
+			print_error( "Option %s has no value", a_name );
+		}
+
+		if ( last_arg ) {
+			add_list_item( a_name, "1", &a_list );
+		}
 	}
 }
 
